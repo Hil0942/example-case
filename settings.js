7f2b24656f7404b7532270afcd9323fe512e09a1
Hil0942 <165234857+Hil0942@users.noreply.github.com>
2024-03-28 11:56:24 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,24 +2,24 @@ const fs = require('fs')
 
 // Api
 global.APIs = {
-	alfa: 'https://api.zeeoneofc.my.id',
+	alfa: 'https://api.HMaRSdVhvtTGDZd.my.id',
 }
 
 // Apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.my.id': 'Your Key',
+	'https://api.HMaRSdVhvtTGDZd.my.id': 'Your Key',
 }
 
 global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
+global.namaowner = "Mas Hel"
+global.packname = Hel032
+global.author = Hel902
 global.myweb = "https://s.id/zeeoneofc"
 global.footer_text = "© " + namabot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
-global.pairingNumber = "" //pairing number to install bot 
+global.owner = ['6285390689215']
+global.pairingNumber = "625390689215" //pairing number to install bot 
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
